b5e1270c2d64d62e7d42bc70fd3b4572765500a1
SyedAli00896 <SyedAli00896@gmail.com>
2020-09-28 19:22:26 +0500

    splash screen

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import {AppText, Screen} from '../../components/common';
-
-import {Color} from '../../constants/Colors';
+import {Image, StyleSheet} from 'react-native';
 
 const Splash = (props) => {
   return (
-    <Screen backgroundColor={Color.background}>
-      <View key="header"></View>
-      <View key="content">
-        <AppText>Splash</AppText>
-      </View>
-    </Screen>
+    <Image
+      style={styles.Image}
+      resizeMode="stretch"
+      source={require('../../assets/images/background.jpg')}
+    />
   );
 };
+const styles = StyleSheet.create({
+  Image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default Splash;
