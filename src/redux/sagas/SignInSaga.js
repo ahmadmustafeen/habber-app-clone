ec0eeca4276b55a8af0a76f44918e70c9a1f421b
SyedAli00896 <SyedAli00896@gmail.com>
2020-10-21 16:04:36 +0500

    signin authorization to device added

--- src/redux/sagas/SignInSaga.js
+++ src/redux/sagas/SignInSaga.js
@@ -1,7 +1,8 @@
+import {put, call} from 'redux-saga/effects';
+
 import {API_ENDPOINTS} from '_constants/Network';
 import {RestClient} from '_network/RestClient';
-import {put, call} from 'redux-saga/effects';
-import {SIGN_IN_FAILURE, SIGN_IN_SUCCESS} from 'redux/actionTypes';
+import {SIGN_IN_FAILURE, SIGN_IN_SUCCESS} from '_redux/actionTypes';
 
 export function* signinSaga({type, payload}) {
   try {
@@ -9,12 +10,18 @@ export function* signinSaga({type, payload}) {
     const response = yield call(() =>
       RestClient.post(API_ENDPOINTS.signin, payload),
     );
-    const {status, data, message} = response;
-    if (status === 200){
+    const {
+      status,
+      data: {data: res},
+      message,
+    } = response;
+    console.log('SIgnIp Saga Response . . . .  .', response);
+
+    if (status === 200) {
+      RestClient.setHeader('Authorization', `Bearer ${res.token}`);
       yield put({type: SIGN_IN_SUCCESS, paylaod: null});
     }
-    console.log('SIgnIp Saga Response . . . .  .', data);
-  
+
     yield put({type: SIGN_IN_SUCCESS, paylaod: null});
   } catch (error) {
     yield put({type: SIGN_IN_FAILURE, error});
