7441d4feab7fd66104659269af33f1b6234c29bd
SyedAli00896 <SyedAli00896@gmail.com>
2020-11-18 17:53:56 +0500

    header image added UI RECTIFIED

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,47 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import { View, StyleSheet, Image, Platform } from 'react-native';
-import { Icon } from 'react-native-elements';
-import { AppText, BackgroundImage } from './common';
-import { useTheme } from '@react-navigation/native';
-import { CART_SCREEN, SEARCH } from 'constants/Screens';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Platform,
+  ImageBackground,
+  I18nManager,
+} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {Icon} from 'react-native-elements';
+import {AppText, BackgroundImage} from './common';
+import {useTheme} from '@react-navigation/native';
+import {CART_SCREEN, SEARCH} from 'constants/Screens';
 
 const Header = (props) => {
-  const { colors } = useTheme();
+  const {colors} = useTheme();
   const {
     navigation,
     title,
     color,
     secondary,
-    route: { name },
+    route: {name},
     headerLeft,
     headerRight,
-    noTitle
+    noTitle,
   } = props;
   const headerColor =
     color || (secondary && colors.secondary) || colors.primary;
   return (
-    <View style={styles.container}>
-      <View>
-        {headerLeft ? (
-          headerLeft
-        ) : (
+    <ImageBackground
+      {...props}
+      style={styles.bgImage}
+      resizeMode={'stretch'}
+      source={require('_assets/images/header.png')}>
+      <View style={styles.container}>
+        <View>
+          {headerLeft ? (
+            headerLeft
+          ) : (
             <Icon
               onPress={() => navigation.openDrawer()}
               color={headerColor}
@@ -33,15 +49,15 @@ const Header = (props) => {
               type="feather"
             />
           )}
-      </View>
+        </View>
 
-      <View style={{ flex: 4, paddingLeft: 10 }}>
-        {!noTitle && <AppText color={headerColor}>{title || name}</AppText>}
-      </View>
-      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        {headerRight ? (
-          headerRight
-        ) : (
+        <View style={{flex: 4, paddingLeft: 10}}>
+          {!noTitle && <AppText color={headerColor}>{title || name}</AppText>}
+        </View>
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          {headerRight ? (
+            headerRight
+          ) : (
             <View style={styles.right}>
               <Icon
                 onPress={() =>
@@ -58,7 +74,7 @@ const Header = (props) => {
                 <Icon
                   props={props}
                   onPress={() =>
-                    navigation.navigate(SEARCH, { label: 'SEARCH', SEARCH })
+                    navigation.navigate(SEARCH, {label: 'SEARCH', SEARCH})
                   }
                   color={headerColor}
                   name="search1"
@@ -67,22 +83,27 @@ const Header = (props) => {
               )}
             </View>
           )}
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 const styles = StyleSheet.create({
+  bgImage: {
+    flex: 1,
+    transform: [{scaleX: I18nManager.isRTL ? -1 : 1}],
+  },
   container: {
-    height: Platform.OS == 'ios' ? 75 : 60,
+    height: Platform.OS == 'ios' ? hp(31) : hp(22),
     flexDirection: 'row',
-    alignItems: 'flex-end',
+    alignItems: 'center',
     justifyContent: 'space-between',
-    paddingBottom: 5,
-    marginBottom: 5,
+    // paddingBottom: 5,
+    // marginBottom: 5,
   },
   right: {
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
 });
-export { Header };
+export {Header};
