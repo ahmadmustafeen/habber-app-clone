ad08486f5adc30be6c0b6a886273822c57ae94a2
Syed Ali <SyedAli@Muhammads-MacBook-Pro.local>
2020-12-01 17:13:39 +0500

    remote notification received

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Navigator from './src/navigator';
-import { navigationRef } from './NavigationService';
-import { StatusBar, View } from 'react-native';
-import { ModalScreen } from 'components';
+import {navigationRef} from './NavigationService';
+import {StatusBar, View} from 'react-native';
+import {ModalScreen} from 'components';
 import useNetworkModal from 'utils/customHooks/useNetworkModal';
 import OfflineNotice from 'components/OfflineNotice';
-import { Color } from 'constants/Colors';
-
+import {Color} from '_constants/Colors';
+import RemotePushController from './src/services/RemotePushController';
 
 const App = () => {
-  const { network, toggleModal } = useNetworkModal();
+  const {network, toggleModal} = useNetworkModal();
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <StatusBar barStyle="light-content" />
       <Navigator ref={navigationRef} />
+
       <ModalScreen
         colors={Color}
         visible={network}
         onContinue={toggleModal}
-        heading='Network Error'
-        description='Kindly check your network and try again'
-        buttonLabel='Check your Network'
+        heading="Network Error"
+        description="Kindly check your network and try again"
+        buttonLabel="Check your Network"
       />
+      <RemotePushController />
       {/* <OfflineNotice /> */}
     </View>
   );
