2b79ff687d0525c86f675b42c4eddb52e16221bb
Syed Ali <SyedAli@Muhammads-MacBook-Pro.local>
2020-12-02 13:09:26 +0500

    notification on android done

--- src/services/RemotePushController.js
+++ src/services/RemotePushController.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from 'react';
 import PushNotification from 'react-native-push-notification';
+import NotifService from '../../NotifService';
 const RemotePushController = () => {
   useEffect(() => {
+    new NotifService();
     PushNotification.configure({
       // (optional) Called when Token is generated (iOS and Android)
       onRegister: function (token) {
-        console.log('TOKEN:', token);
+        console.log('TOKEN:', token.token);
       },
       // (required) Called when a remote or local notification is opened or received
       onNotification: function (notification) {
