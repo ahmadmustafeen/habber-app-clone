39a5e702ad9ed7b1c0a3e51a64eae9dcd0cf3db5
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2020-12-02 19:45:25 +0500

    address saga

--- src/redux/sagas/AddressSaga.js
+++ src/redux/sagas/AddressSaga.js
@@ -5,7 +5,7 @@ import { API_ENDPOINTS } from '_constants/Network';
 import { RestClient } from '_network/RestClient';
 import { MY_PROFILE } from '_constants/Screens';
 import { NETWORK_ERROR, SHOW_NETWORK_MODAL } from 'redux/actionTypes';
-import { ADD_ADDRESS_FAILURE, ADD_ADDRESS_SUCCESS } from '_redux/actionTypes';
+import { ADD_ADDRESS_FAILURE, ADD_ADDRESS_SUCCESS, FETCH_ADDRESS } from '_redux/actionTypes';
 import * as NavigationService from '../../../NavigationService';
 export function* addressSaga({ type, payload }) {
   console.log('Add Address Saga . . . .  .1', payload);
@@ -20,13 +20,14 @@ export function* addressSaga({ type, payload }) {
     const { status, data, message } = response;
     console.log('Addres ADD Books Saga Response . . . .  .', response);
     if (data.success) {
-      yield put({ type: ADD_ADDRESS_SUCCESS, payload: payload });
-      Alert.alert('Successfully Added new Address', message, [
-        {
-          onPress: () =>
-            NavigationService.navigate('MyProfile', { screen: MY_PROFILE }),
-        },
-      ]);
+      yield put({ type: ADD_ADDRESS_SUCCESS, });
+      yield put({ type: FETCH_ADDRESS }),
+        Alert.alert('Successfully Added new Address', message, [
+          {
+            onPress: () =>
+              NavigationService.navigate('MyProfile', { screen: MY_PROFILE }),
+          },
+        ]);
     }
   } catch (error) {
     yield put(errorAction(ADD_ADDRESS_FAILURE, error));
