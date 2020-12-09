80fad9bf3dbd0919aec0627448b59107b6052b97
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2020-12-09 12:48:10 +0500

    cart plus working

--- src/redux/reducers/CartReducer.js
+++ src/redux/reducers/CartReducer.js
@@ -9,7 +9,7 @@ const initialState = {
   total_price: 0,
 };
 
-export default (state = initialState, {type, payload}) => {
+export default (state = initialState, { type, payload }) => {
   switch (type) {
     case UPDATE_CART_ITEM: {
       console.log('Cart Payload', payload);
@@ -43,20 +43,22 @@ export default (state = initialState, {type, payload}) => {
         };
       }
 
-      const updatedState = {...state};
+      const updatedState = { ...state };
       let product = updatedState[payload.product_type][alreadyAvailable];
-      console.log('payload', payload);
+      console.log('payload', payload.action);
       if (payload.action === 'add') {
-        product.cart_quantity < product.product_quantity &&
+        // console.log(product.cart_quantity, product.quantity, "this")
+        (product.cart_quantity < product.quantity) &&
           (product.cart_quantity += 1);
       } else if (payload.action === 'sub') {
+        console.log("agya")
         product.cart_quantity > 0 ? (product.cart_quantity -= 1) : null;
       } else if (payload.action === 'remove') {
         updatedState[payload.product_type].splice(alreadyAvailable, 1);
       }
 
       product.cart_price = payload.price * product.cart_quantity;
-      console.log('BOOK PRICE', updatedState.book);
+      console.log('BOOK PRICE', updatedState.bookmark);
       updatedState.total_price =
         updatedState.book.reduce(
           (total, book) =>
@@ -71,7 +73,7 @@ export default (state = initialState, {type, payload}) => {
 
           0,
         );
-      return {...updatedState};
+      return { ...updatedState };
     }
 
     // case ADD_TO_CART: {
@@ -108,7 +110,7 @@ export default (state = initialState, {type, payload}) => {
 
     case FETCH_USER_CART_SUCCESS: {
       console.log('FETCH_USER_CART_SUCCESS', payload);
-      return {...state, ...payload};
+      return { ...state, ...payload };
     }
     default:
       return state;
