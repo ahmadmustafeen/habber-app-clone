fbccc1db127e9fc4928db9098a2e79ab45a23801
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2020-12-15 13:17:07 +0500

    signout done

--- src/redux/sagas/SignOutSaga.js
+++ src/redux/sagas/SignOutSaga.js
@@ -1,22 +1,23 @@
-import {HOME, SIGNIN_SCREEN} from 'constants/Screens';
-import {getItem, setItem} from 'helpers/Localstorage';
-import {Alert} from 'react-native';
-import {put} from 'redux-saga/effects';
+import { HOME, SIGNIN_SCREEN } from 'constants/Screens';
+import { getItem, setItem } from 'helpers/Localstorage';
+import { Alert } from 'react-native';
+import { put } from 'redux-saga/effects';
 
-import {RestClient} from '_network/RestClient';
-import {SIGN_OUT_SUCCESS, SIGN_OUT_FAILURE} from '_redux/actionTypes';
+import { RestClient } from '_network/RestClient';
+import { SIGN_OUT_SUCCESS, SIGN_OUT_FAILURE } from '_redux/actionTypes';
 import * as NavigationService from '../../../NavigationService';
 export function* signoutSaga() {
   try {
     let userProfile = yield getItem('@userProfile');
     userProfile = JSON.parse(userProfile);
-    let signedOutUserProfile = {language: userProfile.language};
+    let signedOutUserProfile = { language: userProfile.language, currency: userProfile.currency };
+    console.log(signedOutUserProfile, "userProfile");
 
     yield setItem('@userProfile', JSON.stringify(signedOutUserProfile));
     RestClient.setHeader('Authorization', null);
-    yield put({type: SIGN_OUT_SUCCESS, payload: signedOutUserProfile});
+    yield put({ type: SIGN_OUT_SUCCESS, payload: signedOutUserProfile });
     NavigationService.navigate(SIGNIN_SCREEN);
   } catch (error) {
-    yield put({type: SIGN_OUT_FAILURE, error});
+    yield put({ type: SIGN_OUT_FAILURE, error });
   }
 }
