355bb19440b9770f99cb02e7ac44532da398bce6
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2020-12-18 18:52:27 +0500

    eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  plugins: ['import'],
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        alias: {
-          _assets: './src/assets',
-          _components: './src/components',
-          _constants: './src/constants',
-          _containers: './src/containers',
-          _navigators: './src/navigators',
-          _network: './src/network',
-          _redux: './src/redux',
-          _styles: './src/styles',
-          _utils: './src/utils',
-          _helpers: './src/helpers',
-        },
-      },
-    },
+  extends: ['prettier', 'prettier/react', '@react-native-community'],
+  parser: 'babel-eslint',
+  env: {
+    jest: true,
+    node: true,
   },
+  globals: {
+    fetch: false,
+  },
+  plugins: ['prettier'],
 };
