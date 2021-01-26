d048c285239113bef9edc37e177c4ba83c2b4a1a
Burera <burerakhan@gmail.com>
2021-01-26 11:43:14 +0500

    back_button_issue_resolved

--- src/constants/Screens.js
+++ src/constants/Screens.js
@@ -30,5 +30,6 @@ export const TERMS_AND_CONDITIONS_SCREEN = "TermsAndConditionsScreen";
 export const PAYMENT_SCREEN = 'PaymentView';
 export const RESET_PASSWORD_SCREEN = 'ResetPassword';
 export const SIGNUP_TERM_CODITION = 'SignUpTermCondition'
+export const Languagess = 'languagess'
 
 
---
+++ src/containers/Languagess/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import { useTheme } from '@react-navigation/native';
+
+import { AppText, Button, BackgroundImage } from '_components/common';
+import { useDispatch } from 'react-redux';
+import { withDataActions } from '_redux/actions';
+import { SWITCH_LANG } from '_redux/actionTypes';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const Languagess = (props) => {
+  const { navigate } = props.navigation;
+  const { colors } = useTheme();
+  const dispatch = useDispatch();
+  return (
+    <BackgroundImage>
+      <View key="header" style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('_assets/images/Screenshot_Logo.jpg')}
+        />
+      </View>
+      <View key="content" style={{ alignItems: 'center', paddingTop: hp(15) }}>
+        <AppText style={styles.selecttxt}>Select Your Language</AppText>
+        <Button
+          style={styles.btn}
+          round
+          width="60%"
+          background="white"
+          color={colors.secondary}
+          onPress={() =>
+            dispatch(withDataActions({ language: { id: 2, iso: 'en' } }, SWITCH_LANG))
+          }>
+          English
+        </Button>
+        <Button
+          round
+          width="60%"
+          background="white"
+          color={colors.secondary}
+          onPress={() =>
+            dispatch(withDataActions({ language: { id: 1, iso: 'ar' } }, SWITCH_LANG))
+          }>
+          عربى
+        </Button>
+      </View>
+    </BackgroundImage>
+  );
+};
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 100,
+    height: 100,
+    marginVertical: 20,
+    marginTop: '30%',
+    alignSelf: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  selecttxt: {
+    color: '#c27e12',
+    marginBottom: 40,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  btn: {
+    marginBottom: 25,
+  },
+});
+export default Languagess;
